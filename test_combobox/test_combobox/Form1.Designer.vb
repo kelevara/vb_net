70ea00e02bc80bf4872689e2a6642fb7a2f9ecce
kelevara <andrei.kozlovich@yandex.ru>
2018-04-24 21:48:06 +0300

    В combobox при загрузке формы происходит передача данных из БД (столбец "Год рождения", без повторов, отсортированный)

---
+++ test_combobox/test_combobox/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(26, 42)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(308, 194)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ test_combobox/test_combobox/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Dim dBaseCommand As New System.Data.OleDb.OleDbCommand
+    Dim dBaseConnection As New System.Data.OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=new_BD.mdb")
+    Dim DT As New Data.DataTable
+    Dim DA As OleDb.OleDbDataAdapter
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dBaseCommand = New OleDb.OleDbCommand("SELECT DISTINCT [Год рождения] FROM [Общие данные] ORDER BY [Год рождения]", dBaseConnection)
+        dBaseConnection.Open()
+        dBaseCommand.ExecuteNonQuery()
+        DA = New OleDb.OleDbDataAdapter(dBaseCommand)
+        DA.Fill(DT)
+        Me.ComboBox1.Items.Clear()
+
+        For Each r As DataRow In DT.Rows
+            Me.ComboBox1.Items.Add(r(0).ToString)
+        Next
+    End Sub
+End Class
