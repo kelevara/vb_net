b6b5952d2c01069788a5637105ffa1544deb8ec1
kelevara <andrei.kozlovich@yandex.ru>
2018-04-24 23:29:58 +0300

    Добавлен ListBox. Пока не работает

--- test_combobox/test_combobox/Form1.Designer.vb
+++ test_combobox/test_combobox/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Форма переопределяет dispose для очистки списка компонентов.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,24 +20,34 @@ Partial Class Form1
     'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
     'Для ее изменения используйте конструктор форм Windows Form.  
     'Не изменяйте ее в редакторе исходного кода.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(26, 42)
+        Me.ComboBox1.Location = New System.Drawing.Point(262, 55)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 0
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(31, 55)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(211, 199)
+        Me.ListBox1.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(308, 194)
+        Me.ClientSize = New System.Drawing.Size(453, 354)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.ComboBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -46,4 +56,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
 End Class
--- test_combobox/test_combobox/Form1.vb
+++ test_combobox/test_combobox/Form1.vb
@@ -3,8 +3,14 @@
     Dim dBaseConnection As New System.Data.OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=new_BD.mdb")
     Dim DT As New Data.DataTable
     Dim DA As OleDb.OleDbDataAdapter
+    Dim yearOfBornBaby As String
+
+    Private Function Quote(strVariable As String) As String
+        Quote = " ' " & strVariable & " ' "
+    End Function
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DT.Clear()
         dBaseCommand = New OleDb.OleDbCommand("SELECT DISTINCT [Год рождения] FROM [Общие данные] ORDER BY [Год рождения]", dBaseConnection)
         dBaseConnection.Open()
         dBaseCommand.ExecuteNonQuery()
@@ -15,5 +21,26 @@
         For Each r As DataRow In DT.Rows
             Me.ComboBox1.Items.Add(r(0).ToString)
         Next
+        dBaseConnection.Close()
+        DT.Clear()
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim FIO As New ArrayList()
+        DT.Clear()
+        dBaseCommand = New OleDb.OleDbCommand("SELECT [Фамилия] AND [Имя] FROM [Общие данные] WHERE [Год рождения] = " & Quote(ComboBox1.SelectedItem), dBaseConnection)
+        dBaseConnection.Open()
+        dBaseCommand.ExecuteNonQuery()
+        DA = New OleDb.OleDbDataAdapter(dBaseCommand)
+        DA.Fill(DT)
+        ListBox1.MultiColumn = True
+        Me.ListBox1.Items.Clear()
+
+        For Each r As DataRow In DT.Rows
+            FIO.Add(r(0).ToString)
+        Next
+
+        ListBox1.Items.Add(FIO)
+        dBaseConnection.Close()
     End Sub
 End Class
